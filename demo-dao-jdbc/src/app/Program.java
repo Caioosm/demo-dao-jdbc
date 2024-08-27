9f1bd0c3c6b09be04a4326d4dc198667eafcc09c
caioo <matossousacaio.cms@gmail.com>
2024-08-27 18:15:55 -0300

    new class DepartmentDaoJDBC with new methods

--- demo-dao-jdbc/src/app/Program.java
+++ demo-dao-jdbc/src/app/Program.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 // import model.entities.Department;
@@ -15,6 +16,8 @@ public class Program {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
         
         System.out.println("=== TEST 1: seller findByID: ===");     
         SellerDao sellerDao = DaoFactory.creatSellerDao();
@@ -46,12 +49,41 @@ public class Program {
         sellerDao.update(seller);
         System.out.println("Update completed!");
 
-        System.out.println("\n=== TEST 6: seller Delete: ===");
-        System.out.print("Enter ID for delete test: ");
-        int id = sc.nextInt();
-        sellerDao.deleteById(id);
-        System.out.println("Delete completed!");
+        // System.out.println("\n=== TEST 6: seller Delete: ===");
+        // System.out.print("Enter ID for delete test: ");
+        // int id = sc.nextInt();
+        // sellerDao.deleteById(id);
+        // System.out.println("Delete completed!");
+        
+        
+        System.out.println("\n=== TEST 1: Department Insert: ===");
+        Department newDepartment = new Department(null, "D3");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New Department: " + newDepartment.getName());
+        
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(6);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+        
+        System.out.println("\n=== TEST 3: Department FindById: ===");
+        Department dep = departmentDao.findById(1);
+		System.out.println(dep);
 
+        System.out.println("\n=== TEST 4: findAll =======");
+		List<Department> list2 = departmentDao.findAll();
+		for (Department d : list2) {
+			System.out.println(d);
+		}
+
+        System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
+        
         sc.close();
 
     }
