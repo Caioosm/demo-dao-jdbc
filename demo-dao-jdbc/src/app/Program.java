0debcbd107d0b5b9603cbf09d53f875d0a4a0566
caioo <matossousacaio.cms@gmail.com>
2024-08-26 21:55:32 -0300

    findById implementation

--- demo-dao-jdbc/src/app/Program.java
+++ demo-dao-jdbc/src/app/Program.java
@@ -1,6 +1,5 @@
 package app;
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,9 +11,10 @@ public class Program {
         
         Department obj = new Department(1, "books");
 
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-
+        
         SellerDao sellerDao = DaoFactory.creatSellerDao();
+        
+        Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
         
--- demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB5;
+import db.DbException5;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+    
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection){
+        this.connection = connection;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -27,8 +40,44 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+        try {
+            preparedStatement = connection.prepareStatement(
+                "SELECT seller.* ,department.Name as DepName "
+                + "FROM seller INNER JOIN department "
+                + "ON seller.DepartmentId = department.ID "
+                + "WHERE seller.Id = ?"
+            );
+
+            preparedStatement.setInt(1, id);
+            rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+
+                return obj;
+            }
+        } 
+        catch (SQLException e) {
+            throw new DbException5(e.getMessage());
+        }
+        finally{
+            DB5.closeStatemnet(preparedStatement);
+            DB5.closeResultSet(rs);
+        }
+
+        return null;
     }
 
     @Override
